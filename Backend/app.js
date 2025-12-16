84e7a1f989d28aad6dc2125917c4b1252b93d6d8
Hemant Dixit <hemantdixit00000@gmail.com>
2025-12-16 23:04:29 +0530

    New

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // ✅ CORS (Vite frontend)
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:"https://hemuP.netlify.app/admin"||"https://hemuP.netlify.app"||"http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })

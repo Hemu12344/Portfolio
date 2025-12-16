53d6c9d7a7bf4bb3101dfa654a5a9ad109dfb0b3
Hemant Dixit <hemantdixit00000@gmail.com>
2025-12-16 21:43:13 +0530

    Backend Add

---
+++ Backend/DataBase/Mail.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const env = require('dotenv').config();
+const URI=process.env.MONGO_URI
+mongoose.connect("mongodb+srv://HarshAgr:Harsh1234@blogsite.zrwnljt.mongodb.net/MailDB")
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+
+  const schema = mongoose.Schema({
+    name:String,
+    phone:String,
+    email:String,
+    subject:String,
+    message:String
+  });
+  
+  module.exports = mongoose.model('mail', schema);
---
+++ Backend/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+
+const mongoose =require("mongoose");
+
+const MailModel= require("./DataBase/Mail");
+const app = express();
+
+// ✅ Middlewares
+app.use(express.json());
+
+// ✅ CORS (Vite frontend)
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// ✅ Test route
+app.post("/Check",async (req, res) => {
+  const { name, phone, email, subject, message } = req.body;
+
+
+  // Basic validation (backend safety)
+  if (!name || !phone || !email || !subject || !message) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  console.log("Received data:", req.body);
+
+  const newMail=MailModel({
+    name:name, phone:phone, email:email, subject:subject, message:message
+  })
+
+  
+  await newMail.save();
+  res.status(200).json({
+    success: true,
+    message: "Form data received successfully",
+  });
+
+});
+
+
+app.get("/api/admin/contacts", async (req, res) => {
+  try {
+    const contacts = await MailModel.find().sort({ createdAt: -1 });
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch data" });
+  }
+});
+
+// ✅ Server start
+app.listen(5000, () => {
+  console.log("🚀 Server running on http://localhost:5000");
+});
